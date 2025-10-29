496e8554f5f0a8b9621ac8b505347a4a40fbc8d5
virtualheet <heetvavadiya456@gmail.com>
2025-10-29 11:44:16 +0530

    feat: replace Counter with MewToken ERC20 implementation
    The Counter contract was removed and replaced with a new ERC20 token implementation called MewToken. This includes:
    - Basic ERC20 functionality with minting capability
    - Initial supply parameter in constructor
    - Owner tracking
    - Added required OpenZeppelin dependency

---
+++ src/MewToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { ERC20 } from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MewToken is ERC20 {
+    address public owner;
+
+    constructor (uint256 initSupply) ERC20("Mew Token", "MEW") {
+        owner = msg.sender;
+        _mint(msg.sender, initSupply * 10 ** 18);
+    }
+
+    // for anyone can mint token
+    function mint (address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
