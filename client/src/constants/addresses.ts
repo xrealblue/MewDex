9e14ad521b8ac0c2d6a04c02df46f4f2ef606bf5
virtualheet <heetvavadiya456@gmail.com>
2025-11-03 00:25:13 +0530

    refactor(constants): move constants and Web3Provider to constants directory
    Restructure project by relocating

---
+++ client/src/constants/addresses.ts
@@ -0,0 +1,25 @@
+export const UNISWAP_V2_MAINNET_ROUTER02_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+export const UNISWAP_V2_MAINNET_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+
+export const TOKENS = {
+  WETH: {
+    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    symbol: 'WETH',
+    decimals: 18
+  },
+  USDC: {
+    address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    symbol: 'USDC',
+    decimals: 6
+  },
+  DAI: {
+    address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    symbol: 'DAI',
+    decimals: 18
+  },
+  USDT: {
+    address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    symbol: 'USDT',
+    decimals: 6
+  }
+};
